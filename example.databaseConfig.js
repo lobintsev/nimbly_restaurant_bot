ef8858e9d3cbceecc49527c9da3692185f3f8cc8
lobintsev <a.lobintsev@sky-logic.ru>
2023-07-06 13:33:26 +0300

    dsddsd

---
+++ example.databaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+    database: 'bebop_bonus',
+    username: 'root',
+    password: 'Overture1928!@',
+    host: 'localhost',  // No protocol
+    dialect: 'mariadb',
+    port: 3306,  // Integer not string
+  };
