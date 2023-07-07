ed617e691d74f4651fb117f9655ce0e4989c2ffd
lobintsev <a.lobintsev@sky-logic.ru>
2023-07-07 22:21:03 +0300

    121212

--- bot.js
+++ bot.js
@@ -2,14 +2,12 @@ import dotenv from "dotenv";
 dotenv.config();
 import TelegramBot from "node-telegram-bot-api";
 import axios from "axios";
-import bwipjs from "bwip-js";
-import sharp from "sharp";
 import db, { writeData, readData } from "./db.js";
 import Message from "./models/Message.js"; 
+import generateCard from "./functions/generateCard.js";
 
 const BOT = new TelegramBot(process.env.TELEGRAM_TOKEN, { polling: true });
 const TENANT_ID = process.env.TENANT_ID;
-const CARD_LOGO = process.env.CARD_LOGO;
 const ADMIN_CHAT_ID = process.env.ADMIN_CHAT_ID;
 
 let awaitingHelpResponse = new Map(); // A map to hold users that are expected to send a help message
@@ -255,7 +253,7 @@ BOT.on("callback_query", async (query) => {
         console.log(`Sending card to chat: ${chatId}`);
         const data = dataCache.get(phone);
         if (data.cards && data.cards.length > 0) {
-          const barcodeImage = await generateBarcode(
+          const barcodeImage = await generateCard(
             data.cards[0].number,
             data.name,
             data.surname
@@ -302,103 +300,6 @@ function formatData(data) {
   });
 
   return message;
-}
-
-async function generateBarcode(number, name, surname) {
-  if (!number) return null;
-
-  return new Promise((resolve, reject) => {
-    bwipjs.toBuffer(
-      {
-        bcid: "code128",
-        text: number,
-        scale: 4,
-        height: 20,
-        includetext: true,
-        textxalign: "center",
-      },
-      async (err, pngBuffer) => {
-        if (err) {
-          reject(err);
-        } else {
-          try {
-            // Fetch the logo image from a URL
-            const response = await axios.get(CARD_LOGO, {
-              responseType: "arraybuffer",
-            });
-            const logoBuffer = Buffer.from(response.data, "binary");
-            // Resize the images
-            const logoHeight = 200;
-            const resizedLogoBuffer = await sharp(logoBuffer)
-              .resize({ height: logoHeight })
-              .toBuffer();
-            const barcodeHeight = 300;
-            const resizedBarcodeBuffer = await sharp(pngBuffer)
-              .resize({ height: barcodeHeight })
-              .toBuffer();
-
-            // Get the width of the resized images
-            const [logoMetadata, barcodeMetadata] = await Promise.all([
-              sharp(resizedLogoBuffer).metadata(),
-              sharp(resizedBarcodeBuffer).metadata(),
-            ]);
-
-            const logoWidth = logoMetadata.width;
-            const barcodeWidth = barcodeMetadata.width;
-            // Create SVG text
-            const text = `${name} ${surname}`;
-            const svgText = `
-              <svg width="700" height="${barcodeHeight}">
-                <style>
-                  .text { fill: #000; font-size: 50px; font-weight: bold;  font-family: 'Roboto', sans-serif;}
-                </style>
-                <text x="50%" y="50%" text-anchor="middle" class="text">${text}</text>
-              </svg>
-            `;
-            const svgTextBuffer = Buffer.from(svgText);
-
-            // Create a new blank image
-            const baseWidth = 720;
-            const baseHeight = 840;
-            const image = await sharp({
-              create: {
-                width: baseWidth,
-                height: baseHeight,
-                channels: 4,
-                background: { r: 255, g: 255, b: 255, alpha: 1 },
-              },
-            });
-
-            // Composite the images and the SVG text
-            const outputBuffer = await image
-              .composite([
-                {
-                  input: resizedLogoBuffer,
-                  top: 70,
-                  left: Math.round((baseWidth - logoWidth) / 2),
-                },
-                {
-                  input: svgTextBuffer,
-                  top: 250,
-                  left: Math.round(baseWidth - 700),
-                },
-                {
-                  input: resizedBarcodeBuffer,
-                  top: 500,
-                  left: Math.round((baseWidth - barcodeWidth) / 2),
-                },
-              ])
-              .png()
-              .toBuffer();
-
-            resolve(outputBuffer);
-          } catch (error) {
-            reject(error);
-          }
-        }
-      }
-    );
-  });
 }
 
 export default BOT;
