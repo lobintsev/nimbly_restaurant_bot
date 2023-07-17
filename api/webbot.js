8909d540a5144b96dac11a346a2a23b67bbccfb6
lobintsev <a.lobintsev@sky-logic.ru>
2023-07-17 14:19:21 +0300

    sfdfd

--- api/webbot.js
+++ api/webbot.js
@@ -23,12 +23,28 @@ const fetchData = async (chatId, phone, bot) => {
   }
 };
 
-const formatData = (data) => {
-  // Format the data as needed
-  // This is a placeholder - you should replace it with your own code
-  return JSON.stringify(data, null, 2);
-};
+function formatData(data) {
+  let message = "*Ваши данные* \n\n";
 
+  if (data.name && data.surname)
+    message += `Имя: ${escapeMarkdown(data.name)} ${escapeMarkdown(
+      data.surname
+    )} \n`;
+  if (data.phone)
+    message += `Телефон: ${escapeMarkdown(data.phone).replace("\\+", "+")} \n`;
+  if (data.email)
+    message += `E-mail: ${escapeMarkdown(data.email).replace("\\.", ".")} \n\n`;
+
+  message += "*Бонусы*: \n";
+  data.walletBalances?.forEach((balanceObj) => {
+    if (balanceObj.name && balanceObj.balance)
+      message += `${escapeMarkdown(
+        balanceObj.name
+      )}: ${balanceObj.balance.toFixed(2)} \n`;
+  });
+
+  return message;
+}
 
 export default async (request, response) => {
   const bot = new TelegramBot(process.env.TELEGRAM_TOKEN);
@@ -36,38 +52,35 @@ export default async (request, response) => {
 
   if (body.message) {
     const { chat: { id: chatId }, text } = body.message;
-    let user = null; // Declare the user variable here
+
+    const user = await User.findOne({
+      where: {
+        chatId: chatId,
+      },
+    });
 
     if (text === "/start") {
-      user = await User.findOne({
-        where: {
-          chatId: chatId,
-        },
-      });
-
       if (user) {
         await bot.sendMessage(chatId, "Привет! Можете проверить баланс или связаться с нами", createMainMenuKeyboard());
       } else {
         await bot.sendMessage(chatId, "Просим Вас пройти регистрацию.", createRegistrationKeyboard());
       }
     } else if (text === "Лояльность") {
-     
+      if (user) {
         await fetchData(chatId, user.phone, bot);
-    
-    } else {
-      if (!user) {
-        user = await User.findOne({
-          where: {
-            chatId: chatId,
-          },
-        });
+      } else {
+        await bot.sendMessage(chatId, "Просим Вас пройти регистрацию.", createRegistrationKeyboard());
       }
+    } else {
+      if (user) {
+        const userString = JSON.stringify(user, null, 2);
+        const message = `✅ Thanks for your message: *"${text}"*\nUser: \`${userString}\`\nHave a great day! 👋🏻`;
+        const keyboardOptions = createMainMenuKeyboard();
 
-      const userString = JSON.stringify(user, null, 2);
-      const message = `✅ Thanks for your message: *"${text}"*\nUser: \`${userString}\`\nHave a great day! 👋🏻`;
-      const keyboardOptions = createMainMenuKeyboard();
-
-      await bot.sendMessage(chatId, message, { ...keyboardOptions, parse_mode: "Markdown" });
+        await bot.sendMessage(chatId, message, { ...keyboardOptions, parse_mode: "Markdown" });
+      } else {
+        await bot.sendMessage(chatId, "Просим Вас пройти регистрацию.", createRegistrationKeyboard());
+      }
     }
   }
 
@@ -86,4 +99,9 @@ function createMainMenuKeyboard() {
 
 function createRegistrationKeyboard() {
   // Modify this function to return your registration keyboard
+}
+
+function escapeMarkdown(text) {
+  // Add this function to escape Markdown syntax
+  return text.replace(/[_*[\]()~`>#+-=|{}.!]/g, '\\$&');
 }
