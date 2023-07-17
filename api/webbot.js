d644d3cf2c7cacea4c547ee9832c72533cb3d6fb
lobintsev <a.lobintsev@sky-logic.ru>
2023-07-17 12:48:03 +0300

    fdfdfd

--- api/webbot.js
+++ api/webbot.js
@@ -7,44 +7,42 @@ import User from "../models/User.js";
 // Export as an asynchronous function
 // We'll wait until we've responded to the user
 export default async (request, response) => {
-    try {
-        // Create our new bot handler with the token
-        // that the Botfather gave us
-        // Use an environment variable so we don't expose it in our code
-        const bot = new TelegramBot(process.env.TELEGRAM_TOKEN);
+  try {
+      // Create our new bot handler with the token
+      // that the Botfather gave us
+      // Use an environment variable so we don't expose it in our code
+      const bot = new TelegramBot(process.env.TELEGRAM_TOKEN);
 
-        // Retrieve the POST request body that gets sent from Telegram
-        const { body } = request;
-        const chatId = msg.chat.id.toString();
-        const user = await User.findOne({
-          where: {
-            chatId: chatId,
-          },
-        });
-        // Ensure that this is a message being sent
-        if (body.message) {
-            // Retrieve the ID for this chat
-            // and the text that the user sent
-            const { chat: { id }, text } = body.message;
+      // Retrieve the POST request body that gets sent from Telegram
+      const { body } = request;
+   
+      // Ensure that this is a message being sent
+      if (body.message) {
+          // Retrieve the ID for this chat
+          // and the text that the user sent
+          const { chat: { id }, text } = body.message;
 
-            // Create a message to send back
-            // We can use Markdown inside this
-            const message = `✅ Thanks for your message: *"${text}"* *"${chatId}"*\nHave a great day! 👋🏻`;
+          // Stringify the body object
+          const bodyString = JSON.stringify(body, null, 2);
 
-            // Send our new message back in Markdown and
-            // wait for the request to finish
-            await bot.sendMessage(id, message, {parse_mode: 'Markdown'});
-        }
-    }
-    catch(error) {
-        // If there was an error sending our message then we 
-        // can log it into the Vercel console
-        console.error('Error sending message');
-        console.log(error.toString());
-    }
-    
-    // Acknowledge the message with Telegram
-    // by sending a 200 HTTP status code
-    // The message here doesn't matter.
-    response.send('OK');
+          // Create a message to send back
+          // We can use Markdown inside this
+          const message = `✅ Thanks for your message: *"${text}"*\nBody: \`${bodyString}\`\nHave a great day! 👋🏻`;
+
+          // Send our new message back in Markdown and
+          // wait for the request to finish
+          await bot.sendMessage(id, message, {parse_mode: 'Markdown'});
+      }
+  }
+  catch(error) {
+      // If there was an error sending our message then we 
+      // can log it into the Vercel console
+      console.error('Error sending message');
+      console.log(error.toString());
+  }
+  
+  // Acknowledge the message with Telegram
+  // by sending a 200 HTTP status code
+  // The message here doesn't matter.
+  response.send('OK');
 };
