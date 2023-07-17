3298b64526aa374924208d337d4adc02611d8c09
lobintsev <a.lobintsev@sky-logic.ru>
2023-07-17 10:05:36 +0300

    35435

--- api/sqlDatabase.js
+++ api/sqlDatabase.js
@@ -2,21 +2,24 @@ import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
 import pg from 'pg';
 dotenv.config();
+
 const databaseConfig = {
-    database: process.env.DB,
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    host: process.env.DB_HOST,  // No protocol
-    dialect: process.env.DB_DIALECT,
-    port: process.env.DB_PORT,  // Integer not string
+    database: process.env.POSTGRES_DATABASE,
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    host: process.env.POSTGRES_HOST,  // No protocol
+    dialect: 'postgres',
+    port: parseInt(process.env.POSTGRES_PORT, 10),  // Integer not string
     dialectOptions: {
         ssl: {
-          require: true,
-          rejectUnauthorized: false // <<<<<<< YOU NEED THIS TO FIX YOUR ISSUE
+            require: true,
+            rejectUnauthorized: false // <<<<<<< YOU NEED THIS TO FIX YOUR ISSUE
         }
-      }
-  };
-  console.log(databaseConfig);
+    }
+};
+  
+console.log(databaseConfig);
+  
 const sequelize = new Sequelize(databaseConfig);
-
+  
 export default sequelize;
